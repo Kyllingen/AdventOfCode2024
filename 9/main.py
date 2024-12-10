b0d7f462b28eb5b4db0ec89bb68ca2c9c6499638
EH <kyllingen@hotmail.com>
2024-12-10 21:04:14 +0100

    updated day9

--- 9/main.py
+++ 9/main.py
@@ -35,10 +35,24 @@ def fillEmptySlots(blocks: list, empty_slots:list, filled_slots:list):
         
         blocks[empty_index] = blocks[filled_index]
         blocks[filled_index] = -1
-        
-        
-    #return ''.join(str(letter) for letter in blocks_list)  
+   
     return blocks
+
+def fillEmptySlotsContingous(blocks:list, empty_slots:list, filled_slots:list):
+    #NOT DONE YET
+    for i in range(0, len(filled_slots)):
+        
+        filled_index =filled_slots[-(1+i)]
+        empty_index = empty_slots[i]
+        
+        # all numbers contingous
+        if empty_index >= filled_index:
+            break
+        
+        blocks[empty_index] = blocks[filled_index]
+        blocks[filled_index] = -1
+   
+    return blocks    
 
 def sum_block(block: list):
     '''calculate sum by summing number * index'''
@@ -60,6 +74,10 @@ empty_slots = findOccurrences(blocks, True)
 filled_slots = findOccurrences(blocks, False)
 
 reordered_blocks = fillEmptySlots(blocks, empty_slots, filled_slots)
+reordered_blocks_part2 = fillEmptySlots(blocks, empty_slots, filled_slots, True)
 
 total_sum = sum_block(reordered_blocks)
+total_sum2 = sum_block(reordered_blocks_part2)
+
 print(total_sum)
+print(total_sum2)
